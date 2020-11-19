39258bf6c773f25ac978bf117bda757e475e594f
Tejeswar0475 <tejeswar096@gmail.com>
2020-11-19 15:52:28 +0530

    Added code to display head or tail as winner

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,1 +1,19 @@
 #!/bin/bash
+
+tail=0
+head=1
+
+read -p "How many times you want to flip a coin:" flipCount
+for (( i=0 ; i< $flipCount ; i++ ))
+do
+	flipCoinResult=$((RANDOM%2))
+	if [[ $flipCoinResult == $tail ]]
+	then
+        	echo "Tail-$flipCoinResult Winner"
+	elif [[ $flipCoinResult == $head ]]
+	then
+		echo "Head-$flipCoinResult Winner"
+	else
+		echo "Invalid Result"
+	fi
+done
