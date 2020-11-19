ea1949f98d84e79fc3184e2f7f3edcf16c519d89
Tejeswar0475 <tejeswar096@gmail.com>
2020-11-19 16:02:46 +0530

    Added headcount and tailCount to knnow number of times win

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -2,6 +2,8 @@
 
 tail=0
 head=1
+headCount=0
+tailCount=0
 
 read -p "How many times you want to flip a coin:" flipCount
 for (( i=0 ; i< $flipCount ; i++ ))
@@ -10,10 +12,20 @@ do
 	if [[ $flipCoinResult == $tail ]]
 	then
         	echo "Tail-$flipCoinResult Winner"
+		((tailCount++))
 	elif [[ $flipCoinResult == $head ]]
 	then
 		echo "Head-$flipCoinResult Winner"
+		((headCount++))
 	else
 		echo "Invalid Result"
 	fi
 done
+echo "Tail has won $tailCount times"
+echo "Head has won $headCount times"
+
+
+
+
+
+
